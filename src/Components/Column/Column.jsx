b9d3cb519b9465905f12d0af6af28a5669ffd9f3
Ever Gonzalez <edgg72@hotmail.com>
2024-07-15 10:02:10 -0500

    Adding 'isNew' functionality for footer links

--- src/Components/Column/Column.jsx
+++ src/Components/Column/Column.jsx
@@ -1,9 +1,15 @@
+import { NewBadge } from "../../assets/NewBadge";
 
 export const Column = ({ column }) => {
   return (
     <div className="footer__column">
       <p className="footer__column footer__column__title">{column.title}</p>
-      {column.contentArray.map((el, index) => <p key={index}>{el}</p>)}
+      {column.contentArray.map((el, index) => (
+        <div key={index} className="footer__column__row">
+          <p>{el.name}</p>
+          {el.isNew ? <NewBadge /> : null}
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
